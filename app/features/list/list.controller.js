9cf477ed8a69e0f8969c64724a673a059979601d
virelai <szwarnogjustyna@gmail.com>
2016-05-27 17:57:36 +0000

    refactor. adding, removing and change status work

--- app/features/list/list.controller.js
+++ app/features/list/list.controller.js
@@ -8,28 +8,14 @@ export default class ListController {
     let listRef = new Firebase("https://justynatodo.firebaseio.com/items");
     this.owner = "Doe";
     this.items = $firebaseArray(listRef);
-    
-    
-     listRef.once("value", function(allItemsSnapshot) {
-      allItemsSnapshot.forEach(function(itemSnapshot) {
-        // Will be called with a messageSnapshot for each child under the /messages/ node
-        var key = itemSnapshot.key();  // e.g. "-JqpIO567aKezufthrn8"
-        
-        console.log(key);
-
-      });
-    });
-    
-    
-
-    console.log(this.items);
-    
-    // this.addItem();
 
   }
   
   addItem(owner, description)
   {
+
+    if(this.desc == '' || this.owner == '')
+      return;
 
     var listRef = new Firebase('https://justynatodo.firebaseio.com/items');
 
@@ -38,59 +24,34 @@ export default class ListController {
       status: "false",
       description: description
     });
-    this.newTask = '';
+    this.desc = '';
     
   }
 
-removeItem(id)
-{
-  let listRef = new Firebase('https://justynatodo.firebaseio.com/items');
-  console.log(id);
-  listRef.child(id).remove();
-  
-  
-  // listRef.once("value", function(allItemsSnapshot) {
-  //     allItemsSnapshot.forEach(function(itemSnapshot) {
-  //       // Will be called with a messageSnapshot for each child under the /messages/ node
-  //       var key = itemSnapshot.key();  // e.g. "-JqpIO567aKezufthrn8"
-        
-  //       console.log(key);
-  //       var uid = itemSnapshot.child("uid").val();  // e.g. "barney"
-  //       var text = itemSnapshot.child("text").val();  // e.g. "Welcome to Bedrock City!"
-        
-        
-        
-
-  //     });
-  //   });
-  
-}
-
-changeStatus(id)
-{
-
-  let listRef = new Firebase('https://justynatodo.firebaseio.com/items');
-  let itemRef = new Firebase('https://justynatodo.firebaseio.com/items');
-  console.log(itemRef.child(id));
-    // if(checkbox.checked)
-    // itemRef.update({ 'status': 'false' });
-    // else
-    // 
-
-itemRef.once("value", function(itemSnapshot) {
-  //       // Will be called with a messageSnapshot for each child under the /messages/ node
-         var key = itemSnapshot.child(id).val().status;  // e.g. "-JqpIO567aKezufthrn8"
-
-
-if(itemSnapshot.child(id).val().status == 'true')
-itemRef.child(id).update({ 'status': 'false' });
-else
-itemRef.child(id).update({ 'status': 'true' });
-
-});
+  removeItem(id)
+  {
     
-
+    let listRef = new Firebase('https://justynatodo.firebaseio.com/items');
+    listRef.child(id).remove();
   
-}
+  }
+  
+  changeStatus(id)
+  {
+  
+    let listRef = new Firebase('https://justynatodo.firebaseio.com/items');
+    console.log(listRef.child(id));
+  
+    listRef.once("value", function(itemSnapshot) {
+    
+    if(itemSnapshot.child(id).val().status == 'true')
+      listRef.child(id).update({ 'status': 'false' });
+    else
+      listRef.child(id).update({ 'status': 'true' });
+    
+    });
+      
+  }
+  
 }
 
