1f5291cf9e787b2c451d0438fd97b6873ca24972
virelai <szwarnogjustyna@gmail.com>
2016-05-27 17:27:20 +0000

    added scratch of data binding

--- app/features/list/index.js
+++ app/features/list/index.js
@@ -3,10 +3,13 @@ import uirouter from 'angular-ui-router';
 import firebase from 'firebase';
 import angularFire from 'angularfire';
 
+import addItem from './directives/add_item';
+
 import routing from './list.routes';
 import ListController from './list.controller';
 
 export default angular.module('app.list', [uirouter, angularFire])
   .config(routing)
   .controller('ListController', ListController)
+  .directive('addItem', addItem)
   .name;
--- app/features/list/list.controller.js
+++ app/features/list/list.controller.js
@@ -2,13 +2,95 @@
 
 export default class ListController {
     
+    
     constructor($firebaseArray) {
         
-    let ref = new Firebase("https://justyna.firebaseio.com/items");
-    this.items = $firebaseArray(ref);
+    let listRef = new Firebase("https://justynatodo.firebaseio.com/items");
+    this.owner = "Doe";
+    this.items = $firebaseArray(listRef);
     
+    
+     listRef.once("value", function(allItemsSnapshot) {
+      allItemsSnapshot.forEach(function(itemSnapshot) {
+        // Will be called with a messageSnapshot for each child under the /messages/ node
+        var key = itemSnapshot.key();  // e.g. "-JqpIO567aKezufthrn8"
+        
+        console.log(key);
+
+      });
+    });
+    
+    
+
     console.log(this.items);
+    
+    // this.addItem();
 
   }
+  
+  addItem(owner, description)
+  {
 
+    var listRef = new Firebase('https://justynatodo.firebaseio.com/items');
+
+    listRef.push().set({
+      owner: owner,
+      status: "false",
+      description: description
+    });
+    this.newTask = '';
+    
+  }
+
+removeItem(id)
+{
+  let listRef = new Firebase('https://justynatodo.firebaseio.com/items');
+  console.log(id);
+  listRef.child(id).remove();
+  
+  
+  // listRef.once("value", function(allItemsSnapshot) {
+  //     allItemsSnapshot.forEach(function(itemSnapshot) {
+  //       // Will be called with a messageSnapshot for each child under the /messages/ node
+  //       var key = itemSnapshot.key();  // e.g. "-JqpIO567aKezufthrn8"
+        
+  //       console.log(key);
+  //       var uid = itemSnapshot.child("uid").val();  // e.g. "barney"
+  //       var text = itemSnapshot.child("text").val();  // e.g. "Welcome to Bedrock City!"
+        
+        
+        
+
+  //     });
+  //   });
+  
 }
+
+changeStatus(id)
+{
+
+  let listRef = new Firebase('https://justynatodo.firebaseio.com/items');
+  let itemRef = new Firebase('https://justynatodo.firebaseio.com/items');
+  console.log(itemRef.child(id));
+    // if(checkbox.checked)
+    // itemRef.update({ 'status': 'false' });
+    // else
+    // 
+
+itemRef.once("value", function(itemSnapshot) {
+  //       // Will be called with a messageSnapshot for each child under the /messages/ node
+         var key = itemSnapshot.child(id).val().status;  // e.g. "-JqpIO567aKezufthrn8"
+
+
+if(itemSnapshot.child(id).val().status == 'true')
+itemRef.child(id).update({ 'status': 'false' });
+else
+itemRef.child(id).update({ 'status': 'true' });
+
+});
+    
+
+  
+}
+}
+
